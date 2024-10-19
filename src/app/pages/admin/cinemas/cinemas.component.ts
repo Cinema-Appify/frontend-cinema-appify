07167580a69a2ed06417b3a870f7f5bf53433bd8
Jgonzalez-tech <jgonzalezp@ucenfotec.ac.cr>
2024-10-19 12:01:41 -0600

    Integration of Table Components

--- src/app/pages/admin/cinemas/cinemas.component.ts
+++ src/app/pages/admin/cinemas/cinemas.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
+import { RegisterCinemaComponent } from "../register-cinema/register-cinema.component";
 
 @Component({
   selector: 'app-cinemas',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RegisterCinemaComponent],
   templateUrl: './cinemas.component.html',
 })
 export class CinemasComponent {
