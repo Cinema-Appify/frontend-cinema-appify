cab2fac74e6ab82d765fdac98e5b5fc694092089
Jeffry Valverde <jeffry18vf@gmail.com>
2024-10-29 17:54:12 -0600

    Revert "Register cinema room"

--- src/app/Interfaces/Cinema.ts
+++ src/app/Interfaces/Cinema.ts
@@ -1,9 +1,9 @@
 
 export interface Cinema {
-  id: number; 
+  id: number; // Cambia a 'string' si estás usando cadenas
   email: string;
   name: string;
-  password?: string; 
+  password?: string; // Si no lo usas, podrías considerar eliminarlo
   photo?: string | null;
   state: string;
 }
--- src/app/pages/cinema/movies/movies.component.ts
+++ src/app/pages/cinema/movies/movies.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../components/navbar/navbar.component';
-import { RegisterMovieComponent } from "../register-movie/register-movie.component";
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [NavbarComponent, RegisterMovieComponent],
+  imports: [NavbarComponent],
   templateUrl: './movies.component.html',
 })
 export class MoviesComponent {
